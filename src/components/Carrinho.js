f925c547bb5ff1971d37b1804f72b2d11470e752
Isabelar07 <isabelarochas@outlook.com.br>
2020-10-22 13:07:30 -0300

    estilização do home

--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+// const CarrinhoCss = styled.h1`
+//     display: grid;
+//     grid-template-columns: 1fr;
+//     grid-template-rows: 1fr;
+//     border: 1px solid blue;
+// `
+
 class Carrinho extends React.Component{
     render(){
         return(
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,27 +2,38 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Grid = styled.div `
-    display:grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    grid-template-rows:1fr 1fr;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: 1fr;
     border: 1px solid blue;
 `
-const Borda = styled.h2 `
-    border:1px solid black;
-`
+// const Borda = styled.h2 `
+//     border:1px solid black;
+// `
+
+const ImagemCss = styled.img`
+  display: grid;
+  width: 15rem;
+  height: 15rem;
+  `
 
 class Home extends React.Component{
     render(){
         return(
             <Grid>                
-                <Borda>PRODUTO 1</Borda>
+                {/* <Borda>PRODUTO 1</Borda>
                 <Borda>PRODUTO 2</Borda>
                 <Borda>PRODUTO 3</Borda>
                 <Borda>PRODUTO 4</Borda>
                 <Borda>PRODUTO 5</Borda>
                 <Borda>PRODUTO 6</Borda>
                 <Borda>PRODUTO 7</Borda>
-                <Borda>PRODUTO 8</Borda>
+                <Borda>PRODUTO 8</Borda> */}
+               <div> 
+                <ImagemCss src={this.props.fotoProduto} alt={'Imagem do produto'} />
+                <h3>{this.props.descricao}</h3>
+                <h3>{this.props.preco}</h3>
+                </div>
             </Grid>
         )
     }
